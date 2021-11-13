8bb7d36b476109de19a84b0e9fe0de0ff8dfa5be
Matthew Kimball <mekimba6@gmail.com>
2021-11-13 13:42:34 -0700

    adds index and show page specs

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -125,11 +125,3 @@ RSpec.describe 'Discount Index Page', type: :feature do
     end
   end
 end
-# As a merchant
-# When I visit my merchant dashboard
-# Then I see a link to view all my discounts
-# When I click this link
-# Then I am taken to my bulk discounts index page
-# Where I see all of my bulk discounts including their
-# percentage discount and quantity thresholds
-# And each bulk discount listed includes a link to its show page
